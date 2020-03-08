6bbf1b325065857f3da284f91cacf898f9a6faac
lzyer <1139255760@qq.com>
2020-03-08 23:41:41 +0800

    code

---
+++ learning4algorithm/src/main/java/com/learning4algorithm/leetcode/Solution1221.java
@@ -0,0 +1,22 @@
+package com.learning4algorithm.leetcode;
+
+public class Solution1221 {
+
+    public int balancedStringSplit(String s) {
+        if (s == null || s.length() == 1) {
+            return 0;
+        }
+        int lCount = 0, rCount = 0, total = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'L') {
+                lCount++;
+            } else {
+                rCount++;
+            }
+            if (lCount == rCount) {
+                total++;
+            }
+        }
+        return total;
+    }
+}
