0d901999c66a7992bd447b5ac8c823bd4fa783c9
lzyer <1139255760@qq.com>
2020-03-09 21:22:02 +0800

    leetcode打卡

---
+++ learning4algorithm/src/main/java/com/learning4algorithm/leetcode/Solution1374.java
@@ -0,0 +1,21 @@
+package com.learning4algorithm.leetcode;
+
+public class Solution1374 {
+
+    public String generateTheString(int n) {
+        char a = 'a';
+        char b = 'b';
+        StringBuilder str = new StringBuilder();
+        if ((n & 1) == 1) {
+            for (int i = 0; i < n; i++) {
+                str.append(a);
+            }
+        } else {
+            str.append(a);
+            for (int i = 1; i < n; i++) {
+                str.append(b);
+            }
+        }
+        return str.toString();
+    }
+}
