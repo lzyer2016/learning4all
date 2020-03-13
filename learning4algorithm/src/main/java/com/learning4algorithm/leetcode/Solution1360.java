3adfe0ae6422e5c6c1f41ac377db580e910685ab
lzyer <lzyer@foxmail.com>
2020-03-13 23:49:25 +0800

    leetcode打卡

---
+++ learning4algorithm/src/main/java/com/learning4algorithm/leetcode/Solution1360.java
@@ -0,0 +1,14 @@
+package com.learning4algorithm.leetcode;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
+public class Solution1360 {
+
+    public int daysBetweenDates(String date1, String date2) {
+        LocalDate localDate1 = LocalDate.parse(date1, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        LocalDate localDate2 = LocalDate.parse(date2, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        return (int) Math.abs(ChronoUnit.DAYS.between(localDate1, localDate2));
+    }
+}
