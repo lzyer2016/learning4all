076e430be157bfdaffab60cc8ec7d7ba479c24ba
lzyer <lzyer@foxmail.com>
2020-03-17 22:36:49 +0800

    leetcode打卡

---
+++ learning4algorithm/src/main/java/com/learning4algorithm/leetcode/Solution1351.java
@@ -0,0 +1,23 @@
+package com.learning4algorithm.leetcode;
+
+/**
+ * 1351. Count Negative Numbers in a Sorted Matrix
+ */
+public class Solution1351 {
+
+    public int countNegatives(int[][] grid) {
+        if (grid == null)
+            return 0;
+        int ret = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = grid[i].length - 1; j >= 0; j--) {
+                if (grid[i][j] < 0) {
+                    ret++;
+                } else {
+                    break;
+                }
+            }
+        }
+        return ret;
+    }
+}
