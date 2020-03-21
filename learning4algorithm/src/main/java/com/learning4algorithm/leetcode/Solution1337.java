c7f52939e16a938b111668e2797900a010a8149e
lzyer <lzyer@foxmail.com>
2020-03-21 22:17:24 +0800

    leetcode代码

---
+++ learning4algorithm/src/main/java/com/learning4algorithm/leetcode/Solution1337.java
@@ -0,0 +1,47 @@
+package com.learning4algorithm.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * 1337. The K Weakest Rows in a Matrix
+ */
+public class Solution1337 {
+
+    class Node {
+        public int index;
+        public int value;
+
+        public Node(int index, int value) {
+            this.index = index;
+            this.value = value;
+        }
+    }
+
+    public int[] kWeakestRows(int[][] mat, int k) {
+        Node[] nodes = new Node[mat.length];
+        for (int i = 0; i < mat.length; i++) {
+            int value = 0;
+            for (int j = 0; j < mat[i].length; j++) {
+                value += mat[i][j];
+            }
+            nodes[i] = new Node(i, value);
+        }
+        Arrays.sort(nodes, (a, b) -> {
+            if (a.value > b.value) {
+                return -1;
+            }
+            if (a.value < b.value) {
+                return 1;
+            }
+            if (a.value == b.value) {
+                return b.index - a.index;
+            }
+            return 0;
+        });
+        int[] ret = new int[k];
+        for (int i = 0, j = nodes.length - 1; i < k; i++) {
+            ret[i] = nodes[j--].index;
+        }
+        return ret;
+    }
+}
