9d80df46e1718b8f17fe5c070584f9839968f634
lzyer <lzyer@foxmail.com>
2020-03-26 22:55:09 +0800

    leetcode打卡

---
+++ learning4algorithm/src/main/java/com/learning4algorithm/leetcode/Solution1313.java
@@ -0,0 +1,22 @@
+package com.learning4algorithm.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * 1313. Decompress Run-Length Encoded List
+ */
+public class Solution1313 {
+    public int[] decompressRLElist(int[] nums) {
+        int len = 0;
+        for (int i = 0; i < nums.length; i += 2) {
+            len += nums[i];
+        }
+        int[] ret = new int[len];
+        int index = 0;
+        for (int i = 0; i < nums.length; i += 2) {
+            Arrays.fill(ret, index, index + nums[i], nums[i + 1]);
+            index = index + nums[i];
+        }
+        return ret;
+    }
+}
