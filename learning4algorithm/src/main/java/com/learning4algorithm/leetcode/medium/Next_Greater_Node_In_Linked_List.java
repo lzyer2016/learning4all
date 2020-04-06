6e0100b29da2e46687eb112abb95b040fd9f095f
lzyer <lzyer@foxmail.com>
2020-04-06 21:11:19 +0800

    leetcode打卡

--- learning4algorithm/src/main/java/com/learning4algorithm/leetcode/medium/Next_Greater_Node_In_Linked_List.java
+++ learning4algorithm/src/main/java/com/learning4algorithm/leetcode/medium/Next_Greater_Node_In_Linked_List.java
@@ -1,5 +1,9 @@
 package com.learning4algorithm.leetcode.medium;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 public class Next_Greater_Node_In_Linked_List {
 
     class ListNode {
@@ -36,6 +40,26 @@ public class Next_Greater_Node_In_Linked_List {
                 ret[index++] = 0;
             }
             p = p.next;
+        }
+        return ret;
+    }
+
+    public int[] nextLargerNodes2(ListNode head) {
+        List<Integer> list = new ArrayList<>();
+        ListNode p = head;
+        while (p != null) {
+            list.add(p.val);
+            p = p.next;
+        }
+        int len = list.size();
+        int[] ret = new int[len];
+        Stack<Integer> stack = new Stack<>();
+        p = head;
+        for (int i = 0; i < len; i++) {
+            while (!stack.isEmpty() && list.get(stack.peek()) < list.get(i)) {
+                ret[stack.pop()] = list.get(i);
+            }
+            stack.push(i);
         }
         return ret;
     }
