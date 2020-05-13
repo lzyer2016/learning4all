c2480af4380fdeb0ef5bbff08266cd6d11aacdb9
lzyer <lzyer@foxmail.com>
2020-05-13 22:33:26 +0800

    leetcode滑动窗口方式

--- learning4algorithm/src/main/java/com/learning4algorithm/leetcode/medium/Number_of_Sub_arrays_of_Size_K.java
+++ learning4algorithm/src/main/java/com/learning4algorithm/leetcode/medium/Number_of_Sub_arrays_of_Size_K.java
@@ -4,15 +4,17 @@ public class Number_of_Sub_arrays_of_Size_K {
 
     public static int numOfSubarrays(int[] arr, int k, int threshold) {
         int ret = 0;
-        for (int i = 0; i < arr.length - k + 1; i++) {
-            int sum = 0;
-            for (int j = i; j < k + i; j++) {
-                sum += arr[j];
-            }
-            System.out.println(sum);
-            if (sum / k >= threshold) {
+        int n = arr.length;
+        int sum = 0;
+        for (int i = 0; i < k - 1; i++) {
+            sum += arr[i];
+        }
+        for (int i = k - 1; i < n; i++) {
+            sum += arr[i];
+            if (sum / k > threshold) {
                 ret++;
             }
+            sum -= arr[i - k + 1];
         }
         return ret;
     }
