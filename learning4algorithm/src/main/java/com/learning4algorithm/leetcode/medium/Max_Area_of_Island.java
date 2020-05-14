498d093827540e417672b5e46d799ec50920edd1
lzyer <lzyer@foxmail.com>
2020-05-14 22:38:56 +0800

    leetcode打卡

---
+++ learning4algorithm/src/main/java/com/learning4algorithm/leetcode/medium/Max_Area_of_Island.java
@@ -0,0 +1,28 @@
+package com.learning4algorithm.leetcode.medium;
+
+public class Max_Area_of_Island {
+
+    public int maxAreaOfIsland(int[][] grid) {
+        int maxArea = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == 1) {
+                    maxArea = Math.max(maxArea, areaOfIsland(grid, i, j));
+                }
+            }
+        }
+        return maxArea;
+    }
+
+
+    public int areaOfIsland(int[][] grid, int i, int j) {
+        if (i > 0 && i < grid.length && j > 0 && j < grid[0].length && grid[i][j] == 1) {
+            grid[i][j] = 0;
+            return 1 + areaOfIsland(grid, i + 1, j)
+                    + areaOfIsland(grid, i - 1, j)
+                    + areaOfIsland(grid, i, j - 1)
+                    + areaOfIsland(grid, i, j + 1);
+        }
+        return 0;
+    }
+}
