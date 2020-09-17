1a8ea3c160eedd670e9c1681d23d5c6b3b054987
lzyer2016 <lzyer@foxmail.com>
2020-09-17 22:49:42 +0800

    leetcode打卡

---
+++ learning4algorithm/src/main/java/com/learning4algorithm/leetcode/medium/LeetCode_515.java
@@ -0,0 +1,53 @@
+package com.learning4algorithm.leetcode.medium;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class LeetCode_515 {
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public List<Integer> largestValues(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        queue.add(root);
+        List<Integer> ans = new ArrayList<>();
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            int max = Integer.MIN_VALUE;
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                max = Math.max(node.val, max);
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+            ans.add(max);
+        }
+        return ans;
+    }
+}
