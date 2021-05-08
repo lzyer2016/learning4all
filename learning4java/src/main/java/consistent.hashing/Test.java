0944210cd059a984f3bc1b0441221bb6e0b6e7d6
lzyer2016 <ken.liu@pkfare.com>
2021-05-08 08:46:26 +0800

    balance test

--- learning4java/src/main/java/consistent.hashing/Test.java
+++ learning4java/src/main/java/consistent.hashing/Test.java
@@ -1,10 +1,14 @@
 package consistent.hashing;
 
+import java.util.Random;
+import java.util.TreeMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Test {
 
   public static void main(String[] args) {
 
-    ConsistentHashing consistentHash = new ConsistentHashing(2, new MD5Hash());
+    ConsistentHashing consistentHash = new ConsistentHashing(16, new MD5Hash());
     consistentHash.add(new Node("node1", "127.0.0.1", "8080"));
     consistentHash.add(new Node("node2", "127.0.0.2", "8080"));
     consistentHash.add(new Node("node3", "127.0.0.3", "8080"));
@@ -30,6 +34,19 @@ public class Test {
     node2 = consistentHash.getNode("ok");
     System.out.println(node2.getKey());
 
+    System.out.println("------数据均衡测试-------");
+    Random random = new Random();
+    TreeMap<String, AtomicInteger> res = new TreeMap<>();
+    for (int i = 0; i < 100000; i++) {
+      String s = "";
+      for (int j = 0; j < 4; j++) {
+        s += 'a' + random.nextInt(26);
+      }
+      Node myNode = consistentHash.getNode(s);
+      res.putIfAbsent(myNode.getKey(), new AtomicInteger());
+      res.get(myNode.getKey()).incrementAndGet();
+    }
+    System.out.println(res.toString());
 
   }
 }
